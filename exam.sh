02807a73dff651b4111980f12a06223dcbe16007
任宇翔 <21371283@buaa.edu.cn>
2023-03-06 19:48:07 +0800

    v-002 relative path

--- exam.sh
+++ exam.sh
@@ -7,7 +7,7 @@ touch myfile
 echo "2023" > myfile
 mv moveme mydir
 cp copyme mydir
-mv ~/21371283/mydir/copyme ~/21371283/mydir/copied
+mv ./mydir/copyme ./mydir/copied
 cat readme
 gcc bad.c 2>> err.txt
 
@@ -17,13 +17,13 @@ if [ $# -eq 0 ]
 then
 	while [ $i -le 10 ] 
 	do
-		touch ~/21371283/gen/$i.txt
+		touch ./gen/$i.txt
 		let i=i+1
 	done
 else
 	while [ $i -le $n ]
 	do
-		touch ~/21371283/gen/$i.txt
+		touch ./gen/$i.txt
 		let i=i+1
 	done
 fi
